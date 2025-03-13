103554698d41d024a64c5b4409b460e1d2a49765
Andrew Frese <andrewfrese@dhcp-10-105-186-233.wireless.northwestern.private>
2025-03-13 18:51:14 -0500

    adding inventory

---
+++ inventory/lambda_function.py
@@ -0,0 +1,52 @@
+import json
+import boto3
+import os
+import datatier
+
+from configparser import ConfigParser
+
+def lambda_handler(event, context):
+  try:
+    print("**STARTING**")
+    print("**lambda: proj05_inventory*")
+    
+    config_file = 'mealapp-config.ini'
+    os.environ['AWS_SHARED_CREDENTIALS_FILE'] = config_file
+    
+    configur = ConfigParser()
+    configur.read(config_file)
+    
+    rds_endpoint = configur.get('rds', 'endpoint')
+    rds_portnum = int(configur.get('rds', 'port_number'))
+    rds_username = configur.get('rds', 'user_name')
+    rds_pwd = configur.get('rds', 'user_pwd')
+    rds_dbname = configur.get('rds', 'db_name')
+
+    print("**Opening connection**")
+    
+    dbConn = datatier.get_dbConn(rds_endpoint, rds_portnum, rds_username, rds_pwd, rds_dbname)
+    
+    print("**Retrieving data**")
+
+    sql = "SELECT * FROM inventory ORDER BY name";
+    
+    rows = datatier.retrieve_all_rows(dbConn, sql)
+    
+    for row in rows:
+      print(row)
+
+    print("**DONE, returning rows**")
+    
+    return {
+      'statusCode': 200,
+      'body': json.dumps(rows)
+    }
+    
+  except Exception as err:
+    print("**ERROR**")
+    print(str(err))
+    
+    return {
+      'statusCode': 500,
+      'body': json.dumps(str(err))
+    }
